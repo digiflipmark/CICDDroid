706be8714848a2df38f927e508e3e444f7388970
Vanita Rathod <vanita.rathod@inkeysolutions.com>
2023-10-02 23:47:30 +0530

    intial commit

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.test
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // add new feature brnch
+        AppCenter.start(
+            application,
+            "2c8c6b8c-de04-4775-b1f9-3f7110df84de",
+            Analytics::class.java,
+            Crashes::class.java
+        )
     }
 }
