95cb7a2e2074f0f25f2a4154f33603fd3a24759d
Vanita Rathod <vanita.rathod@inkeysolutions.com>
2023-10-03 13:45:33 +0530

    send event

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.example.test
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.test.databinding.ActivityMainBinding
 import com.microsoft.appcenter.AppCenter
 import com.microsoft.appcenter.analytics.Analytics
 import com.microsoft.appcenter.crashes.Crashes
 
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         AppCenter.start(
             application,
@@ -18,5 +23,17 @@ class MainActivity : AppCompatActivity() {
             Analytics::class.java,
             Crashes::class.java
         )
+
+        binding.calculateButton.setOnClickListener {
+            // Crashes.generateTestCrash()
+            val interestRate = binding.interestEditText.text.toString().toFloat()
+            val currentAge = binding.ageEditText.text.toString().toInt()
+            val retirementAge = binding.retirementEditText.text.toString().toInt()
+
+            if (interestRate <= 0) {
+                Analytics.trackEvent("wrong_interest_rate")
+            }
+
+        }
     }
 }
